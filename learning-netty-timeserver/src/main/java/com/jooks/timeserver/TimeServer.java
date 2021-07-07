7730f208669f5d0e2d428b0c5e5c3a6183087726
朱坤帅 <jookunshuai@gmail.com>
2021-07-07 15:07:52 +0800

    add timeserver

--- learning-netty-timeserver/src/main/java/com/jooks/timeserver/TimeServer.java
+++ learning-netty-timeserver/src/main/java/com/jooks/timeserver/TimeServer.java
@@ -1,23 +1,22 @@
-package com.jooks.discardserver;
+package com.jooks.timeserver;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
-public class DiscardServer {
+public class TimeServer {
 
     final private int port;
 
-    public DiscardServer(int port) {
+    public TimeServer(int port) {
         this.port = port;
     }
 
-    public void run() throws Exception {
+    public void run() throws InterruptedException {
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
@@ -26,13 +25,10 @@ public class DiscardServer {
                     .channel(NioServerSocketChannel.class)
                     .childHandler(new ChannelInitializer<SocketChannel>() {
                         @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new DiscardServerHandler());
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new TimeServerHandler());
                         }
-                    })
-                    .option(ChannelOption.SO_BACKLOG, 128)
-                    .childOption(ChannelOption.SO_KEEPALIVE, true);
-
+                    });
             ChannelFuture f = b.bind(port).sync();
 
             f.channel().closeFuture().sync();
@@ -42,12 +38,12 @@ public class DiscardServer {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException {
         int port = 8080;
         if (args.length > 0) {
             port = Integer.parseInt(args[0]);
         }
 
-        new DiscardServer(port).run();
+        new TimeServer(port).run();
     }
 }
